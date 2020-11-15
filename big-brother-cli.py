ba0a7747eea6c922218411d9c0becdf0000477c5
Khaled El-Wazan <frost.griffin@gmail.com>
2020-11-15 23:02:23 +0200

    adding timestamps to heartbeats

--- big-brother-cli.py
+++ big-brother-cli.py
@@ -3,77 +3,22 @@ from threading import Thread
 from globals import Globals
 from device_info import DeviceInfo
 import json
-
+from heartbeat import HeartBeat
+import time
 from event import Event
 
 if __name__ == '__main__':
 
     Globals()
-    dinfo = DeviceInfo('123')
-    dinfo.device_id = '123'
-    dinfo.device_name = 'mmoniem'
-    dinfo.notes = 'notes'
 
-    event = Event(device_info=dinfo,project_name='eurus: peres decomposition')
-    event.progress = 10
-    event.to_finish = 100
-    # print(JSONEncoder.encode(event))
+    device = DeviceInfo(
+        device_id='10', device_name='workstation 10', notes='I don not have any notes!')
+    print(device.toJSON())
 
-    # print(event.toJSON())
-    # print(json.dumps(event.toJSON()))
-    # for i in range(5):
-    # event.run_in_background() 
-    # event.progress = 10
-    # event.run_in_background()
-    threads = []
-    event.progress = 50
-    threads.append(event.run_in_background())
-    print('00')
-    event.progress = 100
-    event.run_in_background() 
-    event.progress = 50
-    threads.append(event.run_in_background())
-    print('11')
-    event.progress = 100
-    event.run_in_background() 
-    event.progress = 50
-    threads.append(event.run_in_background())
-    print('22')
-    event.progress = 100
-    event.run_in_background() 
-    threads.append(event.run_in_background())
-    event.progress = 50
-    threads.append(event.run_in_background())
-    print('33')
-    event.progress = 100
-    event.run_in_background() 
-    event.progress = 50
-    threads.append(event.run_in_background())
-    print('44')
-    event.progress = 100
-    event.run_in_background() 
-    event.progress = 50
-    threads.append(event.run_in_background())
-    print('5')
-    # event.progress = 100
-    # event.run_in_background() 
-    # threads.append(event.run_in_background())
+    while(True):
 
-
-    for i in threads:
-        i.join()
-        print('-')
-
-
-    # cred = credentials.Certificate(
-    #     "big-brother-df0a9-firebase-adminsdk-fv0o6-90519d1867.json")
-    # # firebase_admin.initialize_app(cred)
-    # firebase_admin.initialize_app(cred, {
-    #     'databaseURL': 'https://big-brother-df0a9.firebaseio.com'})
-
-    # # As an admin, the app has access to read and write all data, regradless of Security Rules
-    # project_name = 'eurus-peres decompositions'
-    # projects_root = 'projects'
-    # ref = db.reference('{}/{}'.format(projects_root, project_name))
-    # ref.child('khaled').set('mohamed')
-    # print(ref.get())
+        hb = HeartBeat('10',device)
+        thread = hb.run_in_background()
+        thread.join()
+        print(hb.toJSON())
+        time.sleep(10)
--- heartbeat.py
+++ heartbeat.py
@@ -1,18 +1,25 @@
+import firebase_admin
 from globals import Globals
 from device_info import DeviceInfo
 from mother import Mother
 from firebase_admin import db
 import threading
+import time
 
 class HeartBeat(Mother):
 
-    ROOT = 'heart_beats'
+    ROOT = 'heartbeats'
 
-    def __init__(self, device_info: DeviceInfo = None) -> None:
+    def __init__(self, id:str,device_info: DeviceInfo ) -> None:
         self.device_info = device_info.toJSON()
+        self.timestamp=timestamp()
+        self.id = id
 
     def publish(self):
-        gs = Globals()
-        gs.init()
+        # gs = Globals()
+        # gs.init()
         db.reference(HeartBeat.ROOT).child(
             self.device_info['device_id']).set(self.toJSON())
+            
+def timestamp():
+    return int(round(time.time()*1000))
